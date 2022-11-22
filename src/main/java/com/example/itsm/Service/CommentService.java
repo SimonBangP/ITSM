18ffc03822464a15d9d0167b9f405d9867d51312
Simon Bang <simo12k1@stud.kea.dk>
2022-11-22 13:59:12 +0100

    Created Repository and Service for Comment Entity

---
+++ src/main/java/com/example/itsm/Service/CommentService.java
@@ -0,0 +1,10 @@
+package com.example.itsm.Service;
+
+import com.example.itsm.Repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CommentService {
+
+    @Autowired
+    CommentRepository commentRepository;
+}
