e1d1932a656e12be9e2119c8d33e32babcb37e6c
OmerMert32 <omermert@live.dk>
2022-11-23 11:22:42 +0100

    account unittest lavet

--- src/main/java/com/example/itsm/Entity/Comment.java
+++ src/main/java/com/example/itsm/Entity/Comment.java
@@ -1,7 +1,9 @@
 package com.example.itsm.Entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -9,6 +11,8 @@ import javax.persistence.ManyToOne;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Comment {
