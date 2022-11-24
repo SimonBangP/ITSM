0d25cbc0b80f51f21e89b42b8d16ed27ebf3ff3f
OmerMert32 <omermert@live.dk>
2022-11-24 13:05:17 +0100

    alle klasser ift comment er oprettet og lavet

--- src/main/java/com/example/itsm/API/CommentController.java
+++ src/main/java/com/example/itsm/API/CommentController.java
@@ -1,10 +1,25 @@
 package com.example.itsm.API;
 
+import com.example.itsm.DTO.response.AccountResponse;
+import com.example.itsm.DTO.response.CommentResponse;
+import com.example.itsm.Repository.CommentRepository;
+import com.example.itsm.Service.CommentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/comments")
 public class CommentController {
 
+    CommentService commentService;
+
+    @GetMapping
+    public ResponseEntity<List<CommentResponse>> getComments(){
+        return new ResponseEntity<>(commentService.getComments(), HttpStatus.OK);
+    }
 }
